daf8b3503b99b4f7db803f5766c0e5c0e7739611
buuhsixx <brunooalbuquerque16@hotmail.com>
2019-09-02 15:39:32 -0300

    mudanças no index

--- Perfil.php
+++ Perfil.php
@@ -9,11 +9,11 @@ if(!$_SESSION['logado'] && !(isset($_SESSION['user']) && !empty($_SESSION['user'
 header("location:index.php");
 }
 $id = $_SESSION['id'];
-$barrar = sprintf("SELECT id_usuario  FROM formcorp WHERE id_usuario=$id");
-$dados = mysqli_query( $conexao,$barrar) or die(mysqli_error());
+$barrar = sprintf("SELECT id_usuario FROM formcorp WHERE id_usuario=$id");
+$dados = mysqli_query($conexao,$barrar) or die(mysqli_error());
                        $num = mysqli_fetch_array($dados,MYSQLI_NUM)[0];
-                                      if ($num != 1){
-                                        echo '<script> window.location.replace("FormCorp.php"); </script>';
+                                      if ($num != $id){
+                                      echo '<script> window.location.replace("FormCorp.php"); </script>';
                                       }
 ?>
   <body>
